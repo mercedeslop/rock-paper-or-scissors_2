a9ebaf7377d3abba0bd1b1c1ae318ecbf8bdb5bf
mercedeslop <jenifersasa2004@gmail.com>
2023-11-09 17:15:20 -0500

    add base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/mercedeslop/rock-paper-or-scissors_2/'
+  base: 'https://mercedeslop.github.io/rock-paper-or-scissors_2/'
 })
